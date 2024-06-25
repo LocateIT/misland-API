f50a52a9973a1ee6d93e3cb3efe5a5597c80ac9b
Grace Amondi <miswa.grace@gmail.com>
2024-06-25 18:13:42 +0300

    refactor mailing service

--- misland_api/config/base.py
+++ misland_api/config/base.py
@@ -14,12 +14,15 @@ SETTINGS = {
         'EE_PRIVATE_KEY': os.getenv('EE_PRIVATE_KEY'),
         'EE_SERVICE_ACCOUNT': os.getenv('EE_SERVICE_ACCOUNT'),
         'EE_SERVICE_ACCOUNT_JSON': os.getenv('EE_SERVICE_ACCOUNT_JSON'),
-        'SPARKPOST_API_KEY': os.getenv('SPARKPOST_API_KEY'),
         'API_URL': os.getenv('API_URL'),
         'API_ADMIN_EMAIL': os.getenv('API_ADMIN_EMAIL'),
         'API_PASSWORD': os.getenv('API_PASSWORD'),
         'EMAIL_USER':os.getenv('EMAIL_USER'),
-        'EMAIL_PASSWORD':os.getenv('EMAIL_PASSWORD')
+        'EMAIL_PASSWORD':os.getenv('EMAIL_PASSWORD'),
+        'EMAIL_SERVER':os.getenv('EMAIL_SERVER'),
+        'EMAIL_PORT':os.getenv('EMAIL_PORT'),
+        'EMAIL_USE_TLS':os.getenv('EMAIL_USE_TLS'),
+        'EMAIL_USE_SSL':os.getenv('EMAIL_USE_SSL')
     },
     'ROLES': ['ADMIN', 'USER'],
     'SQLALCHEMY_DATABASE_URI': 'postgresql://'+os.getenv('POSTGRES_USER')+':'+os.getenv('POSTGRES_PASSWORD')+'@'+os.getenv('DATABASE_PORT_5432_TCP_ADDR')+':'+os.getenv('DATABASE_PORT_5432_TCP_PORT')+'/'+os.getenv('POSTGRES_DB'),
